0ebcbfcf40bdcd2964c3cc627285116ac19779d9
James Lywood <james.lywood@gmail.com>
2025-02-03 17:24:34 -0500

    Fixed Synchronized

--- server.js
+++ server.js
@@ -25,8 +25,9 @@ bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
 //START_SYNC
 
 var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
-var result = bcrypt.compareSync(myPlaintextPassword, hash);
 console.log(hash);
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result);
 
 //END_SYNC
 
